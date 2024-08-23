3bd1ed134f6e0a67031d50c0d6cc7d3e4d00383e
ProfeAdrianJ <doc_aa_jaimes@fesc.edu.co>
2024-08-23 10:05:44 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ images.g.ts
@@ -0,0 +1,130 @@
+// Código generado automáticamente. No editar.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "myImage0":return img`
+. . . . . 3 3 . . . . . . . . . 
+. . . 3 3 . 3 . . . . . . . . . 
+. 3 3 . . 3 . 3 3 3 3 . . . . . 
+3 3 3 . 3 3 3 . . . . 3 3 . . . 
+1 3 . 3 . 3 f f f . . . . 3 . . 
+1 3 . f f f f f f . . . . 3 . . 
+. 3 f f 3 . f f f f f f . 3 . . 
+. 3 3 . 3 . . f f f f f . 3 . . 
+3 3 . 3 . . 3 . f . . . . 3 . . 
+. . . f 3 3 . . f f f . 3 . . . 
+. . f . . f f f f f f 3 . . . . 
+. . f f f 3 . f f . 3 . . . . . 
+. . . . . 3 . f 3 3 . . . . . . 
+. . 2 2 . 3 3 . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim0":
+            case "anim2":return [img`
+..........666666666666..........
+........6667777777777666........
+......66677777777777777666......
+.....6677777779999777777766.....
+....667777779966669977777766....
+....677777799668866117777776....
+...66777779966877861197777766...
+...66777799668677686699777766...
+...88777796688888888669777788...
+...88777788888888888888777788...
+...88977888679999997688877988...
+...88977886777777777768877988...
+...88997777777777777777779988...
+...88799777777777777777711788...
+...88679997777777777779117688...
+..cc866679999999999999976668cc..
+.ccbc6666679999999999766666cbcc.
+.fcbcc66666666666666666666ccbcf.
+.fcbbcc666666666666666666ccbdcf.
+.f8bbbccc66666666666666cccbddcf.
+.f8cbbbbccccccccccccccccbdddbcf.
+.f8ccbbbbbccccccccccccb111ddccf.
+.f6ccccbbbddddddddddddd111dcccf.
+.f6ccccccbbddddddddddddddbbcccf.
+.f6cccccccccccccbbbbbbbbbdbcccf.
+..f6cccccccccbbbbbbbbbbbddbccf..
+..f6cccccccccbbbbbbbbbbbddbccf..
+..ff6ccccccccbbbbbbbbbbbddbcff..
+...ff6cccccccbbbbbbbbbbbddbff...
+....ffcccccccbbbbbbbbbbbdbff....
+......ffccccbbbbbbbbbbbbff......
+........ffffffffffffffff........
+`, img`
+................................
+................................
+................................
+................................
+................................
+..........888888888888..........
+........8887777777777888........
+......88877666666666677888......
+.....8877666667777666667788.....
+....887666667788887766666788....
+....866666677888888996666678....
+...88666667788877889976666688...
+...88666677888677688877666688...
+...88666778888888888887766688...
+...88667788888888888888776688...
+..cc866788866777777668887668cc..
+.ccbc8668866666666666688668cbcc.
+.fcbcc86666666666666666668ccbcf.
+.fcbbcc886666666666666688ccbdcf.
+.f8bbbccc88888888888888cccbddcf.
+.f8cbbbbccccccccccccccccbdddbcf.
+.f8ccbbbbbccccccccccccb11dddccf.
+.f6ccccbbbdddddddddddd111ddcccf.
+.f6ccccccbbddddddddddd11dbbcccf.
+.f6cccccccccccccbbbbbbbbbdbcccf.
+..f6cccccccccbbbbbbbbbbbddbccf..
+..f6cccccccccbbbbbbbbbbbddbccf..
+..ff6ccccccccbbbbbbbbbbbddbcff..
+...ff6cccccccbbbbbbbbbbbddbff...
+....ffcccccccbbbbbbbbbbbdbff....
+......ffccccbbbbbbbbbbbbff......
+........ffffffffffffffff........
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
